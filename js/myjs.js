9d0696f289385bf2997e89f58c551cc3b8e45278
wrciar <warren.ciar029@gmail.com>
2023-10-26 08:39:25 +0800

    tables

--- js/myjs.js
+++ js/myjs.js
@@ -4,3 +4,14 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => {
 	nav.classList.toggle("navclose");
 })
+
+
+BuildDataTable('#testTable');
+function BuildDataTable(dt)
+{
+  var table = $(dt).DataTable({
+    order: [[1, 'asc']],
+    "scrollX": true,
+    "ordering": true,
+  }).buttons().container().appendTo(dt+'_wrapper .col-md-6:eq(0)');
+}
